e1bd131132115165213d2d76e6b8bf8a2afff685
rousseltom13-bit <rousseltom13@gmail.com>
2026-04-28 23:38:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v31';
+const CACHE = 'atlas-v32';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
