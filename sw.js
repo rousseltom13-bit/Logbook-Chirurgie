5cdc0b0c5f87d83e16846d855ef12ec0b55bfdf1
rousseltom13-bit <rousseltom13@gmail.com>
2026-04-28 08:09:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v17';
+const CACHE = 'atlas-v27';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
