19753b5f6c17372a7bfcd8845c6acaf0c4b58c74
rousseltom13-bit <rousseltom13@gmail.com>
2026-09-05 07:53:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'atlas-v148';
+const CACHE = 'atlas-v150';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
